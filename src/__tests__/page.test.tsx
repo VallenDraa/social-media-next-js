988cded8e65eab25affc771793e3f2dddd54a960
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-04-08 21:09:54 +0700

    test: setup jest for unit and integration testing

---
+++ src/__tests__/page.test.tsx
@@ -0,0 +1,14 @@
+import Home from '@/app/page';
+import { render, screen } from '@testing-library/react';
+
+describe('Page', () => {
+	it('renders a heading', () => {
+		render(<Home />);
+
+		const headings = screen.getAllByRole('heading', { level: 2 });
+
+		for (const heading of headings) {
+			expect(heading).toBeInTheDocument();
+		}
+	});
+});
